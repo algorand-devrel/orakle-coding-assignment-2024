edcf9b08d9f0e0cc7bf5d6bb42656a952419592f
iskysun96 <iskysun96@gmail.com>
2024-05-25 15:53:55 +0900

    pera debug on

--- projects/orakle-nft-marketplace-app-frontend/src/App.tsx
+++ projects/orakle-nft-marketplace-app-frontend/src/App.tsx
@@ -40,6 +40,7 @@ export default function App() {
       nodeToken: String(algodConfig.token),
     },
     algosdkStatic: algosdk,
+    debug: true,
   })
 
   return (
--- projects/orakle-nft-marketplace-app-frontend/src/main.tsx
+++ projects/orakle-nft-marketplace-app-frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/main.css'
 import ErrorBoundary from './components/ErrorBoundary'
+import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
