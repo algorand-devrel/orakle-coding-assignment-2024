51613941ad40f1c3617c8eed0b910ef646d3dfef
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-06-01 18:00:59 +0900

    feat: implement all problems

--- projects/orakle-nft-marketplace-app-frontend/src/utils/marketplaceListAppId.ts
+++ projects/orakle-nft-marketplace-app-frontend/src/utils/marketplaceListAppId.ts
@@ -1,1 +1,1 @@
-export const marketplaceListAppId = 1018 //테스트 중 에러가 발생하면 마켓플레이스 리스트 스마트계약을 재배포하고 새로운 app id로 교체해주세요.
+export const marketplaceListAppId = 1020 //테스트 중 에러가 발생하면 마켓플레이스 리스트 스마트계약을 재배포하고 새로운 app id로 교체해주세요.
